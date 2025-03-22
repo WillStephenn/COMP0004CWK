bcbd24d03cf6a48da3d7563514ed4b11e8029e35
William Stephen <zcabwst@ucl.ac.uk>
2025-03-22 11:33:31 +0000

    Started implementing Note management methods in the model class; load, save and get notes.

--- src/main/java/uk/ac/ucl/model/Model.java
+++ src/main/java/uk/ac/ucl/model/Model.java
@@ -1,45 +1,50 @@
 package uk.ac.ucl.model;
 
-import java.io.Reader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
+import uk.ac.ucl.model.domain.Note;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.io.File;
 
 public class Model
 {
-  // The example code in this class should be replaced by your Model class code.
-  // The data should be stored in a suitable data structure.
+  private List<Note> notes;
+  private final ObjectMapper mapper = new ObjectMapper();
+  private final String notesFilePath = "data/notes.json";
 
-  public List<String> getPatientNames()
-  {
-    return readFile("data/patients100.csv");
-  }
-
-  // This method illustrates how to read csv data from a file.
-  // The data files are stored in the root directory of the project (the directory your project is in),
-  // in the directory named data.
-  public List<String> readFile(String fileName)
-  {
-    List<String> data = new ArrayList<>();
-
-    try (Reader reader = new FileReader(fileName);
-         CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT))
-    {
-      for (CSVRecord csvRecord : csvParser)
-      {
-        // The first row of the file contains the column headers, so is not actual data.
-        data.add(csvRecord.get(0));
+  public List<Note> loadNotes() {
+    try {
+      File file = new File(notesFilePath);
+      if (!file.exists()) {
+        return new ArrayList<>();
       }
-    } catch (IOException e)
-    {
+      notes = mapper.readValue(file, new TypeReference<List<Note>>(){});
+      return notes;
+    } catch (IOException e) {
       e.printStackTrace();
+      return new ArrayList<>();
     }
-    return data;
   }
+
+  public Boolean saveNotes(){
+    try {
+      mapper.writeValue(new File(notesFilePath), notes);
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+    return false;
+    }
+  }
+
+  public List<Note> getNotes(){
+    return notes;
+  }
+
+
+
+
 
   // This also returns dummy data. The real version should use the keyword parameter to search
   // the data and return a list of matching items.
